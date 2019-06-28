8f7c7d3175bafab8d77df31b0ce79d2fc1581ae1
Jeremy Rodi <me@retroc.at>
2019-06-27 18:18:59 -0700

    More Redis Storage Stuff

--- src/storage/items.rs
+++ src/storage/items.rs
@@ -1,6 +1,5 @@
 use crate::storage::Sealed;
 use failure::Error;
-use futures::{Future, Stream};
 use std::collections::{HashMap, HashSet};
 use uuid::Uuid;
 
@@ -10,6 +9,12 @@ pub struct Item {
     views: u64,
     popularity: f64,
     meta: HashMap<String, HashSet<String>>,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct ItemList {
+    pub items: Vec<(Uuid, f64)>,
+    pub nmods: u32,
 }
 
 impl Item {
@@ -33,4 +38,6 @@ pub trait ItemStorage: Sealed {
     fn find_items_near(&self, item: Uuid) -> Result<Vec<Uuid>, Error>;
     fn find_items_top(&self) -> Result<Vec<Uuid>, Error>;
     fn find_items_popular(&self) -> Result<Vec<Uuid>, Error>;
+
+    fn items_add_near(&self, item: Uuid, near: Uuid) -> Result<(), Error>;
 }
--- src/storage/redis.rs
+++ src/storage/redis.rs
@@ -1,18 +1,53 @@
-use crate::storage::items::{Item, ItemStorage};
+use crate::storage::items::{Item, ItemList, ItemStorage};
 use crate::storage::sealed::Sealed;
 use crate::storage::Storage;
 use config::Config;
 use failure::Error;
-use r2d2::Pool;
-use r2d2_redis::redis::Commands;
+use r2d2::{Pool, PooledConnection};
+use r2d2_redis::redis::{Commands, PipelineCommands};
 use r2d2_redis::RedisConnectionManager;
 use uuid::Uuid;
+use std::ops::DerefMut;
 
-pub struct RedisStorage(Pool<RedisConnectionManager>);
+struct RedisKeys;
+
+impl RedisKeys {
+    fn item(id: Uuid) -> String {
+        format!("item:{}", id)
+    }
+
+    fn item_near(id: Uuid) -> String {
+        format!("item:{}:near", id)
+    }
+
+    const fn all_items_top() -> &'static str {
+        "item:!:top"
+    }
+
+    const fn all_items_popular() -> &'static str {
+        "item:!:pop"
+    }
+}
+
+pub struct RedisStorage {
+    pool: Pool<RedisConnectionManager>,
+    near_modification_max: u32,
+    near_list_size_max: u32,
+}
 
 impl RedisStorage {
     pub fn load(config: &Config) -> RedisStorage {
         unimplemented!()
+    }
+
+    fn calc_near(&self, item_list: &mut ItemList) -> Result<(), Error> {
+        if item_list.nmods > self.near_modification_max {
+            item_list.items = item_list.items.into_iter().take(self.near_list_size_max as usize).map(|(item, count)| {
+                (item, count.ln())
+            }).collect();
+        }
+
+        Ok(())
     }
 }
 
@@ -21,8 +56,8 @@ impl Storage for RedisStorage {}
 
 impl ItemStorage for RedisStorage {
     fn find_item(&self, item: Uuid) -> Result<Option<Item>, Error> {
-        let conn = self.0.get()?;
-        let contents: Option<Vec<u8>> = conn.get(format!("item:{}", item))?;
+        let conn = self.pool.get()?;
+        let contents: Option<Vec<u8>> = conn.get(RedisKeys::item(item))?;
         contents
             .map(|content| bincode::deserialize::<Item>(&content))
             .transpose()
@@ -30,30 +65,54 @@ impl ItemStorage for RedisStorage {
     }
 
     fn find_items_near(&self, item: Uuid) -> Result<Vec<Uuid>, Error> {
-        let conn = self.0.get()?;
-        Ok(conn
-            .zscan::<_, String>("item:{}:near")?
-            .flat_map(|item| Uuid::from_str(&item).ok()))
+        let conn = self.pool.get()?;
+        let item_list = load_item_near(conn.get(RedisKeys::item_near(item))?)?;
+        Ok(item_list.items.into_iter().map(|(item, _)| item).collect())
     }
 
-    fn add_near(&self, current: Uuid, other: Uuid) -> Result<(), Error> {
-        let conn = self.0.get()?;
-
+    fn items_add_near(&self, current: Uuid, other: Uuid) -> Result<(), Error> {
+                let conn = self.pool.get()?;
+        let key = RedisKeys::item_near(current);
+        loop {
+            redis::cmd("WATCH").arg(&key).query(conn)?;
+            // TODO
+        }
     }
 
     fn find_items_top(&self) -> Result<Vec<Uuid>, Error> {
-        let conn = self.0.get()?;
+        let conn = self.pool.get()?;
         Ok(conn
-            .zscan::<_, String>("item:!:top")?
-            .flat_map(|item| Uuid::from_str(&item).ok()))
+            .zscan::<_, String>(RedisKeys::all_items_top())?
+            .flat_map(|item| item.parse::<Uuid>().ok())
+            .collect())
     }
 
     fn find_items_popular(&self) -> Result<Vec<Uuid>, Error> {
-        let conn = self.0.get()?;
+        let conn = self.pool.get()?;
         Ok(conn
-            .zscan::<_, String>("item:!:pop")?
-            .flat_map(|item| Uuid::from_str(&item).ok()))
+            .zscan::<_, String>(RedisKeys::all_items_popular())?
+            .flat_map(|item| item.parse::<Uuid>().ok())
+            .collect())
     }
 }
 
-fn calc_near(conn: impl Commands, )
+fn load_item_near(contents: Option<Vec<u8>>) -> Result<ItemList, Error> {
+        Ok(contents
+            .map(|content| bincode::deserialize::<ItemList>(&content))
+            .transpose()?
+            .unwrap_or_default())
+}
+
+fn inner_add_near(this: &RedisStorage, conn: &mut PooledConnection<RedisConnectionManager>, key: &str, other: Uuid) -> Result<Option<()>, Error> {
+    let list = load_item_near(conn.get(key)?)?;
+    if let Some((_, count)) = list.items.iter_mut().find(|(id, _)| other == *id) {
+        *count += 1.0;
+    } else {
+        list.items.push((other, 1.0));
+    }
+
+    list.nmods += 1;
+    this.calc_near(&mut list)?;
+    let content = bincode::serialize(&list)?;
+    redis::pipe().atomic().set(key, content).query(conn.deref_mut()).map_err(Error::from)
+}
